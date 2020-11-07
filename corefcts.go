960cc0a346376b39615be5b533fcaa1a41d0c6c7
fabdem <demurger@gmail.com>
2020-11-06 18:28:20 -0800

    WIP

--- corefcts.go
+++ corefcts.go
@@ -68,162 +68,32 @@ func (v *VDFFile) SkipHeader(buf []byte) (res []byte) {
 	return res
 }
 
+
+// Parse all key/values in a map
 func (v *VDFFile) Parse(buf []byte) (m_token map[string]string, err error) {
-	v.log(fmt.Sprintf("Parse"))
+	v.log(fmt.Sprintf("Parse()"))
 
-	var tokenPattern = regexp.MustCompile(`(?mi)^\s*"[a-z]{1,15}"\s*\{`)
-
-
+	var pairPattern = regexp.MustCompile(`(?mi)(?:^\s*)("[a-z\d_:#\$]{1,}")\s*?"[^"\\]*(?:\\.[^"\\]*)*"`)
+	
+	if pairPattern == nil {
+		return m_token, errors.New(fmt.Sprintf("Parse() - no match"))
+	}
+	
+	m_token = make(map[string]string)
+	
+	kvPairs := pairPattern.FindAllSubmatch(buf, -1)
+	for _, kv := range kvPairs {
+		key := string(kv[0])
+		value := string(kv[1])
+		m_token[key] = value
+	}
+	return m_token, nil
 }
 
-/*
-// Define a split function that separates strings (anything between valid double quotes).
-func onDblQuotes(data []byte, atEOF bool) (advance int, token []byte, err error) {
-	if err != nil {
-		log.Fatalf("Error while scanning: %s ", err)
-	}
-	var beg int
-	var i int
 
-	// Seek the opening double quote or '//'
-	for {
-		// Is this a double quote?
-		if bytes.Equal(data[i:i+len(cDbleQuote)], cDbleQuote) {
-			beg = i
-			break
-		}
-
-		// Is this a '//'?
-		if (i <= len(data)-len(cDbleSlash)) && bytes.Equal(data[i:i+len(cDbleSlash)], cDbleSlash) {
-			// Skip everything until we get to a line feed
-			i += len(cDbleSlash)
-			for {
-				if i >= len(data)-1 {
-					return 0, data[0:1], bufio.ErrFinalToken // last line: empty
-				}
-				if bytes.Equal(data[i:i+len(cLineFeed)], cLineFeed) {
-					beg = i + len(cLineFeed)
-					break
-				}
-				i++
-			}
-		}
-
-		i++
-		if i >= len(data)-1 {
-			return 0, data[0:1], bufio.ErrFinalToken // last line: empty
-		}
-	}
-
-	// Seek the closing (non escaped) double quote
-	for i = beg + len(cDbleQuote); i < len(data)-1; i++ {
-		if bytes.Equal(data[i:i+len(cDbleQuote)], cDbleQuote) && !bytes.Equal(data[i-len(cBackSlash):i], cBackSlash) {
-			return i + len(cDbleQuote), data[beg : i+len(cDbleQuote)], nil // Fine this is the end of the token
-		}
-	}
-	// There is one final line to be delivered, which is an empty string.
-	// Returning bufio.ErrFinalToken here tells Scan there are no more tokens after this
-	// but does not trigger an error to be returned from Scan itself.
-	return 0, data[0:1], bufio.ErrFinalToken
-}
-*/
-
-/*
-// Parse the file into a map
 //
-//
-func (v *VDFFile) ParseFile() (fileMapped map[string]string, err error) {
-	v.log(fmt.Sprintf("ParseFile(%s)", v.fileName))
-
-	fs, err := v.f.Stat() // Get file size
-	if err != nil {
-		return fileMapped, errors.New(fmt.Sprintf("Can't read file size : %v", err))
-	}
-
-	err = SkipBom()
-	if err != nil {
-		return fileMapped, err
-	}
-
-	// Skip headers for both files
-	err = skipHeader()
-	if err != nil {
-		return fileMapped, err
-	}
-
-	// Create a map and populate with file content - using string as index but it's actually []byte
-	fileMapped = make(map[string]string)
-
-	scannerf := bufio.NewScanner(v.f)
-
-	//adjust the capacity (file max characters) - the default size is 4096 bytes!!!!
-	maxCapacity := fi1.Size() + 1024
-	buf := make([]byte, maxCapacity)
-	scannerf.Buffer(buf, int(maxCapacity))
-
-	// Set the split function
-	scannerf.Split(v.onDblQuotes)
-
-	// Populate map with the tokens key/value pairs
-	for {
-		if !scannerf.Scan() {
-			break
-		}
-		key := scannerf.Bytes()
-
-		if !scannerf.Scan() {
-			break
-		}
-		value := scannerf.Bytes()
-
-		fileMapped[string(key)] = value
-	}
-
-	emptyFile := true
-
-	// Compare 2nd file with the map and build the diff file
-	for {
-		if !scannerf1.Scan() {
-			break
-		}
-		key := scannerf1.Bytes()
-		key1 := make([]byte, len(key)) // Weird the fileMapped modifies var value - had to make a copy in order to use the var within the if
-		copy(key1, key)
-
-		if !scannerf1.Scan() {
-			break
-		}
-		value := scannerf1.Bytes()
-		value1 := make([]byte, len(value))
-		copy(value1, value)
-
-		if !tokenOnlyFlg {
-			combined := append(key, value...)
-			if !fileMapped[string(combined)] {
-				if emptyFile {
-					emptyFile = false
-					fmt.Printf("%s", bom) // Add a bom if needed
-				}
-				// It's a diff -> print out
-				fmt.Printf("%s%s%s%s", key1, cTab, value1, cCRLF)
-			}
-
-		} else { // Option -t used (compares token names only)
-			if !fileMapped[string(key)] {
-				if emptyFile {
-					emptyFile = false
-					fmt.Printf("%s", bom) // Add a bom if needed
-				}
-				// It's a diff -> print out
-				fmt.Printf("%s%s%s%s", key1, cTab, value1, cCRLF)
-			}
-		}
-	}
-
-	// If file content is the same say so in UTF8
-	if emptyFile {
-		fmt.Printf("File contents are identical or %s is a subset of %s\n", firstFileName, secondFileName)
-	}
-
-}
-*/
+func (v *VDFFile) GetEncoding() (string) {
+	v.log("GetEncoding()")
+	return v.encoding
+}	
+	
